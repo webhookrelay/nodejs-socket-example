a0124219eb324d3832f11a622798896d1e17b625
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-12-03 22:06:08 +0000

    updated example with reconnect

--- client.js
+++ client.js
@@ -1,23 +1,35 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('wss://my.webhookrelay.com/v1/socket');
+var server = 'wss://my.webhookrelay.com/v1/socket';
+var reconnectInterval = 1000 * 3;
+var ws;
 
 var apiKey = process.env.RELAY_KEY;
 var apiSecret = process.env.RELAY_SECRET;
 
-ws.on('open', function open() {
-  console.log('connected, sending authentication request');
-  ws.send(JSON.stringify({action: 'auth', key: apiKey, secret: apiSecret}));  
-});
+var connect = function(){
+    ws = new WebSocket(server);
+    ws.on('open', function() {        
+        console.log('connected, sending authentication request');
+        ws.send(JSON.stringify({ action: 'auth', key: 'aa', secret: apiSecret }));
+    });
 
-ws.on('close', function close() {
-  console.log('disconnected');
-});
+    ws.on('message', function incoming(data) {
+      console.log(data)
+      var msg = JSON.parse(data);
+      if (msg.type === 'status' && msg.status === 'authenticated') {
+        ws.send(JSON.stringify({ action: 'subscribe', buckets: ['123'] }));
+      }
+    });
 
-ws.on('message', function incoming(data) {
-  console.log(data)
-  var msg = JSON.parse(data);
-  if (msg.type === 'status' && msg.status === 'authenticated') {
-    ws.send(JSON.stringify({action: 'subscribe', buckets: ['123']}));
-  }
-});
+    ws.on('error', function() {
+        console.log('socket error');       
+    });
+
+    ws.on('close', function() {
+        console.log('socket closed, reconnecting');
+        setTimeout(connect, reconnectInterval);
+    });
+};
+
+connect();
